dc19bfc19b2a7b0096b841e73f81f665382e4960
MateuszLisowski <mlmateuszlisowski@gmail.com>
2018-08-01 22:08:13 +0200

    writing tests in progress

---
+++ src/components/Cart/CartItems/CartItems.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CartItems from './CartItems';
+import { shallow } from 'enzyme';
+
+describe('<CartItems />', () => {
+    //const wrapper = shallow(<CartItems cartItems={} buyItems={() => { }} removeItem={() => { }} />);
+    it('work', () => {
+        expect(1).toBe(1);
+    })
+})
